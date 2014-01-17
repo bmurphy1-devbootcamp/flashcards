b70e88c409aea2521532a89daaac50a4301d2da6
dbc-apprentice <apprentice@devbootcamp.com>
2014-01-17 14:27:09 -0600

    refactor controller

--- flashcards.rb
+++ flashcards.rb
@@ -1,38 +1,23 @@
 # Main file for 
 
-require 'csv'
 require_relative 'models'
 require_relative 'views'
 
 class Controller
 
   def run
-    deck = Deck.new
-    View.parse(filename).each do |card|
-      deck.add_card(card)
-    end
-
-    # show welcome view
-    # loop until quit
-      # decide what card to show
-      # show first card
-      # check user input for correct answer
-      # delete card from deck if answer is correct
-    # 
     View.welcome
-    until deck.has_cards? == false
-      deck.shuffle
+    deck.shuffle
+    deck.each_card do |card|
       current_card = deck.next_card
       View.show_card(current_card)
-      until View.input == current_card.answer
+      until current_card.correct?(View.input)
         View.try_again
       end
       View.correct
     end
     View.finished
-
-
-
-end
+  end
 
 end
+
